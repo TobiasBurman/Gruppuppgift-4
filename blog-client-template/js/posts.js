23ebe6d461456ef5d1367e2323313acb8045d994
Freddan <f.eriksson86@gmail.com>
2022-12-10 16:01:05 +0100

    Some styling done :)

--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -8,13 +8,15 @@ async function getPosts(){
         let blogPosts=""
         for (let posts of data){
             blogPosts +=`
-            <h3>${posts.title}</h3>
-            <i>${posts.author}</i>
-            <i>${posts.date}</i>
-            <div>
-                <p class="text">${posts.content.slice(0,200)}...<a href="post.html?id=${posts._id}" data-id=${posts._id}>Read more</a></p>
-                <p class="tags">Tags: ${posts.tags.join(', ')}</p>
-             </div>   
+            <section class="box">
+                <h3>${posts.title}</h3>
+                <i>Author ${posts.author}</i>
+                <i>${posts.date}</i>
+                <div>
+                    <p class="text">${posts.content.slice(0,200)}...<a href="post.html?id=${posts._id}" data-id=${posts._id}>Read more</a></p>
+                    <p class="tags">Tags: ${posts.tags.join(', ')}</p>
+                </div>   
+            </section>   
             `
             document.getElementById("content").innerHTML=blogPosts
         }
