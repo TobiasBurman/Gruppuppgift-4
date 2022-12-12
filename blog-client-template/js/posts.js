c061e66b0195a5991d665fb86eb76eeecbba9cf1
Fredrik Eriksson <f.eriksson86@gmail.com>
2022-12-12 15:38:29 +0100

    fix

--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -9,13 +9,12 @@ async function getPosts(){
         for (let posts of data){
             blogPosts +=`
             <section class="box">
-                <h3>${posts.title}</h3>
-                <i>Author ${posts.author}</i>
-                <i>${posts.date}</i>
-                <div>
-                    <p class="text">${posts.content.slice(0,200)}...<a href="post.html?id=${posts._id}" data-id=${posts._id}>Read more</a></p>
-                    <p class="tags">Tags: ${posts.tags.join(', ')}</p>
-                </div>   
+                <h1>${posts.title}</h1>
+                <i>Author: ${posts.author}</i>
+                <i>${posts.date.slice(0,16)}</i>
+                <p class="text">${posts.content.slice(0,200)}...<a href="post.html?id=${posts._id}" data-id=${posts._id}>Read more</a></p>
+                <p class="tags">Tags: ${posts.tags.join(', ')}</p>
+             
             </section>   
             `
             document.getElementById("content").innerHTML=blogPosts
